891337416a29d6b1f895d9fdad6cec64f9f75620
Armaan-J02 <armaanj@nexgeniots.com>
2023-07-26 12:08:16 +0530

    join now working perfenctly

--- src/pages/joinnow/joinnow.js
+++ src/pages/joinnow/joinnow.js
@@ -1,60 +1,77 @@
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import './joinnow.css';
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import { Box, Button, Divider, Paper } from "@mui/material";
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
-
-
-function JoinNow(props) {
-  const [username, setUsername] = useState('');
+function JoinNow() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const navigate = useNavigate();
+
   const handleJoinNow = (e) => {
     e.preventDefault();
+    // Add your join now logic here
   };
 
+  function isPasswordValid(password) {
+    const alphanumericRegex = /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[!@#$%^&*])[A-Za-z0-9!@#$%^&*]+$/;
+    return alphanumericRegex.test(password);
+  }
+
+  function arePasswordsMatched() {
+    return password === confirmPassword;
+  }
+
+  function isEmailValid(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  const isJoinNowDisabled = !isPasswordValid(password) || !arePasswordsMatched() || !isEmailValid(email);
 
   return (
-    <div className="jpage">
-      <h2>Join Now</h2>
-      <form onSubmit={handleJoinNow}>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </label>
-        <label>
-          Email Address:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <button type="submit">Join Now</button>
-      </form>
-    </div>
+    <Box className="jpage" component="form" onSubmit={handleJoinNow}>
+      <Typography variant="h2" align="center">
+        Join Now
+      </Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+        <TextField
+          label="Email Address"
+          variant="outlined"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        {!isEmailValid(email) && <Typography color="error">Please enter a valid email address.</Typography>}
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        {password && (
+          <TextField
+            label="Confirm Password"
+            variant="outlined"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        )}
+        {!isPasswordValid(password) && (
+          <Typography color="error">Password should contain a mix of alphanumeric and special characters.</Typography>
+        )}
+        {!arePasswordsMatched() && <Typography color="error">Passwords do not match.</Typography>}
+        <Button type="submit" variant="contained" disabled={isJoinNowDisabled}>
+          Join Now
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
