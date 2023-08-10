f9809dc736e390cf7f5c93d5f01e582ef103fe3b
Armaan-J02 <armaanj@nexgeniots.com>
2023-08-10 16:32:38 +0530

    name is getting extracted just fine!

--- src/models/parser-resume/libs/parser.py
+++ src/models/parser-resume/libs/parser.py
@@ -27,55 +27,28 @@ def extract_text(filepath):
         text = textract.process(filepath).decode('utf-8')
     return text.lower()
 
-def extract_information(text):
-    extracted_info = {}
-    
-     # Extract first name
-    first_name_pattern = r"(?i)(?:\b(?:mr|ms|miss|mrs)\.\s*)?([a-z]+(?: [a-z]+)*)\b"
-    first_name_match = re.search(first_name_pattern, text)
-    if first_name_match:
-        extracted_info['first_name'] = first_name_match.group(1)
+def extract_name(text):
+    name = ""
+    # Extract name using the pattern
+    name_matches = re.findall(patterns['regular']['name'][0], text, re.IGNORECASE)
+    if name_matches:
+        name = ' '.join(name_matches[0])
+    return name
 
-    # Extract Sex/Gender
-    gender_pattern = r"(?i)(male|female|other)"
-    extracted_info['gender'] = re.findall(gender_pattern, text)
-    
-    # Extract email addresses
-    email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
-    extracted_info['email'] = re.findall(email_pattern, text)
-    
-    # Extract phone numbers
-    phone_pattern = r"\b(?:\d{10}|\d{3}-\d{3}-\d{4}|\(\d{3}\)\s*\d{3}-\d{4})\b"
-    extracted_info['phone'] = re.findall(phone_pattern, text)
-    
-
-    # Extract LinkedIn profiles
-    linkedin_pattern = r"(?i)linkedin\.com\/in\/([a-z0-9-]+)"
-    extracted_info['linkedin'] = re.findall(linkedin_pattern, text)
-    
-    # Extract GitHub profiles
-    github_pattern = r"(?i)github\.com\/([a-z0-9-]+)"
-    extracted_info['github'] = re.findall(github_pattern, text)
-    
-    # Extract Twitter profiles
-    twitter_pattern = r"(?i)twitter\.com\/([a-z0-9_]+)"
-    extracted_info['twitter'] = re.findall(twitter_pattern, text)
-    
-    instagram_pattern = r"(?i)instagram\.com\/([a-z0-9_]+)"
-    facebook_pattern = r"(?i)facebook\.com\/([a-z0-9_]+)"
-    extracted_info['instagram'] = re.findall(instagram_pattern, text)
-    extracted_info['facebook'] = re.findall(facebook_pattern, text)
-    
-    
-    return extracted_info
-
-input_filename = 'Abiral_Pandey_Fullstack_Java.docx'  # Replace this with the actual input filename
+input_filename = 'Ernani_Joppert.pdf'  # Replace this with the actual input filename
 input_filepath = os.path.join('storage/inputresume', input_filename)
 resume_text = extract_text(input_filepath)
-info = extract_information(resume_text)
+name = extract_name(resume_text)
+print("Name:", name)
 
-output_filename = input_filename.split('.')[0] + '_info.json'
+parsed_info = {
+    "name": name
+}
+
+# Define the output filename based on the input filename
+output_filename = f"{input_filename.split('.')[0]}_parsed.json"
 output_filepath = os.path.join('storage/output', output_filename)
 
+# Save the extracted information to a JSON file
 with open(output_filepath, 'w') as output_file:
-    json.dump(info, output_file, indent=2)
+    json.dump(parsed_info, output_file, indent=2)
