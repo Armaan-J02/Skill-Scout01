455faf74922a4978a56350450f3b087113cdd1a7
Armaan-J02 <armaanj@nexgeniots.com>
2023-08-11 12:22:46 +0530

    extracting data just fine. Now adding the code to extract name and contact info.

--- src/models/parser-resume/ractor.py
+++ src/models/parser-resume/ractor.py
@@ -3,13 +3,16 @@ import docx2txt
 import PyPDF2
 import textract
 import string
+from patterns import patterns
+from skills import skills
+import re
 
-def extract_text(filepath):
-    """
-    Extract text from a file using its file extension.
-    Split the extracted text into paragraphs using newline characters as delimiters.
+""" 
+    This extract_text function will extract text from a file using its file extension, 
+    split the extracted text into paragraphs using newline characters as delimiters,
     Remove unnecessary punctuations from the extracted text.
-    """
+"""
+def extract_text(filepath):
     extension = os.path.splitext(filepath)[1][1:].lower()
     if extension == 'txt':
         with open(filepath, 'r') as f:
@@ -24,17 +27,77 @@ def extract_text(filepath):
     else:
         text = textract.process(filepath).decode('utf-8')
 
-    # Split text into paragraphs using newline characters as delimiters
-    paragraphs = text.split('\n\n')  # Split by double newline to represent paragraphs
+    # Splitting text into paragraphs using newline characters as delimiters
+    paragraphs = text.split('\n\n') # Split by double newline to represent paragraphs
     
     # Removing unnecessary punctuations from the extracted text.
     translator = str.maketrans('', '', string.punctuation)
     paragraphs = [paragraph.translate(translator) for paragraph in paragraphs]
     
     return paragraphs
+#------------------------------------------------------------
+'''
+    Defining a function whose funciton would be to consider the paragraph or sentence as the content under that heading when match is found!
+'''
+
+def extract_content(paragraphs, patterns_dict):
+    """
+    Extract headings and content based on the provided patterns.
+    """
+    extracted_data = {}
+    current_heading = None
+    
+    for paragraph in paragraphs:
+        for heading, keywords in patterns_dict["titles"].items():
+            for keyword in keywords:
+                if keyword in paragraph.lower():
+                    current_heading = heading
+                    break
+        
+        if current_heading:
+            # Store the paragraph as content under the current heading
+            if current_heading not in extracted_data:
+                extracted_data[current_heading] = paragraph
+            else:
+                extracted_data[current_heading] += '\n' + paragraph
+        
+        current_heading = None  # Reset the current_heading
+    
+    return extracted_data
+
+'''This function is extracting name from the resume using the regulare expression 
+defined in the patterns dictionary in the patterns.py module.''' 
+def extract_name(text):
+    name = ""
+    # Extract name using the pattern
+    name_matches = re.findall(patterns['regular']['name'][0], text, re.IGNORECASE)
+    if name_matches:
+        name = ' '.join(name_matches[0])
+    return name
+
+'''This function is extracting email from the resume using the email_pattern logic
+defined in this function itself!'''
+def extract_email(text):
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    matches = re.findall(email_pattern, text)
+    if matches:
+        return matches[0]
+    else:
+        return ""
+
+
 
 # Example usage
-filepath = "storage/inputresume/Adelina_Erimia_PMP1.docx"
-paragraphs = extract_text(filepath)
-for paragraph in paragraphs:
-    print(paragraph)
+filepath = "storage/inputresume/rohit.txt"
+paragraphs = extract_text(filepath) 
+
+
+content = extract_content(paragraphs, patterns)
+for heading, content in content.items():
+    print(heading,"\n",content,"\n")
+
+
+
+
+
+
